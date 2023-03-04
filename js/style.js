50182ba8665daab3c8ba7474735a69ac6f2af4a4
dany koudede <koudededany@icloud.com>
2023-03-04 03:28:12 -0600

    initial commit

---
+++ js/style.js
@@ -0,0 +1,10 @@
+function openFunction(){
+    document.getElementById("menu").style.width="300px";
+    document.getElementById("mainbox").style.marginLeft="300px";
+    document.getElementById("mainbox").innerHTML="Click on Cross Element and Close Menu";
+   }
+  function closeFunction(){
+   document.getElementById("menu").style.width="0px";
+   document.getElementById("mainbox").style.marginLeft="0px";
+   document.getElementById("mainbox").innerHTML="&#9776; Open";
+  }
